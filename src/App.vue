41de79a50a2920fd7e44ecde20e8dae0a550424a
Taha CEKEN <taha-ceken@hotmail.com>
2022-06-16 21:18:55 +0300

    realtime database support

--- src/App.vue
+++ src/App.vue
@@ -30,17 +30,18 @@
           <div class="columns is-mobile is-vcentered">
             <div
               class="column"
-              :class="{'has-text-success line-through' : todo.done}"
-              >
-              {{
-              todo.content
-              }}</div
+              :class="{ 'has-text-success line-through': todo.done }"
             >
+              {{ todo.content }}
+            </div>
             <div class="column is-5 has-text-right">
-             <button
-             @click="toggleDone(todo.id)"
-                class="button" :class="todo.done ? 'is-success' : 'is-ligth'">&check;</button
+              <button
+                @click="toggleDone(todo.id)"
+                class="button"
+                :class="todo.done ? 'is-success' : 'is-ligth'"
               >
+                &check;
+              </button>
               <button
                 @click="deleteTodo(todo.id)"
                 class="button is-danger ml-2"
@@ -56,49 +57,66 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { v4 as uuidv4 } from 'uuid';
+import { ref, onMounted } from 'vue';
+import {
+  collection,
+  onSnapshot,
+  doc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  query,
+  orderBy,
+} from 'firebase/firestore';
+import { db } from '@/firebase';
 
-const todos = ref([
-  // {
-  //   id: 'id1',
-  //   content: 'wash my ass',
-  //   done: false,
-  // },
-  // {
-  //   id: 'id2',
-  //   content: 'clean my ass',
-  //   done: true,
-  // },
-]);
+const todosCollection = collection(db, 'todos');
+const todosQuery = query(todosCollection, orderBy('date', 'desc'));
+
+const todos = ref([]);
+
+onMounted(async () => {
+  onSnapshot(todosQuery, (querySnapshot) => {
+    const localTodos = [];
+    querySnapshot.forEach((doc) => {
+      const todo = {
+        id: doc.id,
+        content: doc.data().content,
+        done: doc.data().done,
+      };
+      localTodos.push(todo);
+    });
+    todos.value = localTodos;
+  });
+});
 
 const newTodoContent = ref('');
 
 const addTodo = () => {
-  const newTodo = {
-    id: uuidv4(),
+  addDoc(todosCollection, {
     content: newTodoContent.value,
     done: false,
-  };
-  todos.value.unshift(newTodo);
+    date: Date.now(),
+  });
   newTodoContent.value = '';
 };
 
 const deleteTodo = (id) => {
-  todos.value = todos.value.filter((todo) => todo.id !== id);
+  deleteDoc(doc(todosCollection, id));
 };
 
 const toggleDone = (id) => {
-  const index = todos.value.findIndex(todo => todo.id === id);
-  todos.value[index].done = !todos.value[index].done
-}
+  const index = todos.value.findIndex((todo) => todo.id === id);
 
+  updateDoc(doc(todosCollection, id), {
+    done: !todos.value[index].done,
+  });
+};
 </script>
-
 
 <style>
 @import 'bulma/css/bulma.min.css';
-*{
+* {
   transition: all 0.4s;
 }
 body {
